40aca095e4d788cc14a9a05d5507fa6d81654888
Artem Daniliants <artem.daniliants@lumolink.com>
2021-02-09 14:18:08 +0200

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello heroku!"
+    return "Hello heroku again!"
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
