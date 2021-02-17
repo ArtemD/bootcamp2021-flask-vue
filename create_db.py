f8950fe4f99c84477a6c2c30bff9ef6c98ece09e
Artem Daniliants <artem.daniliants@lumolink.com>
2021-02-17 13:29:08 +0200

    Started working on front-end

--- create_db.py
+++
@@ -1,2 +0,0 @@
-from database import *
-Base.metadata.create_all(db)
--- gunicorn_config.py
+++
@@ -1,4 +0,0 @@
-bind = "0.0.0.0:8080"
-""" Run gunicorn on all ips and on port 8080 """
-workers = 2
-""" Start 2 worker processes """
